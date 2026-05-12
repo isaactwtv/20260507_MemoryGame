650dc21471c3dfd011cb5d5d2cb76e26de043246
isaactwtv <isaactwtv@gmail.com>
2026-05-12 17:10:02 +0800

    修正白畫面

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: './',
+  base: '/20260507_MemoryGame/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
